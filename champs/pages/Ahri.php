e53052c7e444e4d6cbf02508ff454ae5fea8f731
Bryan Oswald <bearsfan221@gmail.com>
2014-11-21 07:14:14 -0600

    forgot quotes

--- champs/pages/Ahri.php
+++ champs/pages/Ahri.php
@@ -6,7 +6,7 @@
 	<?php include '/var/www/php-riot-api.php'; ?>
 	<?php $region = 'na'; 
 		$instance = new riotapi($region); ?>
-	<?php $champdata = $instance->getStatic($call='champion', $id=103?champData=all); ?> 
+	<?php $champdata = $instance->getStatic($call='champion', $id=103'?champData=all'); ?> 
 </head>
 <body>
 	<div class="champ_pic">
--- champs/pages/Nidalee.php
+++ champs/pages/Nidalee.php
@@ -6,7 +6,7 @@
 	<?php include '/var/www/php-riot-api.php'; ?>
 	<?php $region = 'na'; 
 		$instance = new riotapi($region); ?>
-	<?php $champdata = $instance->getStatic($call='champion', $id=76?champData=all); ?> 
+	<?php $champdata = $instance->getStatic($call='champion', $id=76'?champData=all'); ?> 
 </head>
 <body>
 	<div class="champ_pic">
